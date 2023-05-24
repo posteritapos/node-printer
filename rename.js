d94913a6e5c153a27cbae3a62ed77a1b92ff7350
posteritapos <124765986+posteritapos@users.noreply.github.com>
2023-05-24 12:09:32 +0400

    Update rename.js

--- rename.js
+++ rename.js
@@ -4,7 +4,7 @@ const path = require("path");
 const oldPath =  path.resolve(__dirname,'lib','node_printer.node');
 const newPath = path.resolve(__dirname,'lib',`node_printer_${process.platform}_${process.arch}.node`);
 
-fs.rename(oldPath, newPath, (err) => {
+fs.copyFile(oldPath, newPath, (err) => {
   if (err) {
     console.error('Error renaming file:', err);
   } else {
